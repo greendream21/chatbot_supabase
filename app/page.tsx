ee88a3dab44be84f2339405229917aa085b72033
= <hornet.dev.0802@gmail.com>
2023-08-31 11:53:47 -0400

    # update

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const resources = [
     icon: "M7 4V20M17 4V20M3 8H7M17 8H21M3 12H21M3 16H7M17 16H21M4 20H20C20.5523 20 21 19.5523 21 19V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V19C3 19.5523 3.44772 20 4 20Z",
   },
   {
-    title: "Supabase Next.js App Router Example",
+    title: "Supabase",
     subtitle:
       "Want to see a code example containing some common patterns with Next.js and Supabase? Check out this repo!",
     url: "https://github.com/supabase/supabase/tree/master/examples/auth/nextjs",
