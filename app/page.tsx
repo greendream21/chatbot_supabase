86df3ca73788b2ef53ef9a3f415c4168d7a4355f
= <hornet.dev.0802@gmail.com>
2023-08-31 12:38:50 -0400

    Coauth
    Co-authored-by: Hornet-Developer john.henry.9901111@gmail.com

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,7 @@ const resources = [
   },
   {
     title: "Supabase",
-    subtitle:
-      "Want to see a code example containing some common patterns with Next.js and Supabase? Check out this repo!",
+    subtitle: "Want",
     url: "https://github.com/supabase/supabase/tree/master/examples/auth/nextjs",
     icon: "M10 20L14 4M18 8L22 12L18 16M6 16L2 12L6 8",
   },
